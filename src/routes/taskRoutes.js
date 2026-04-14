176191bd29789557360191f38928ca13b53314d1
Tiana Turner <tianasturner.17@gmail.com>
2026-04-14 16:57:39 -0400

    apply middleware to route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import * as taskController from '../controllers/taskController.js';
-import { validateTask } from '../middleware/validateTask.js';
+import { validateTask, validateGetTasks } from '../middleware/validateTask.js';
 
 const router = express.Router();
 
-router.get('/', taskController.getTasks);
+router.get('/', validateGetTasks, taskController.getTasks);
 router.post('/', validateTask, taskController.createTask);
 
 export default router;

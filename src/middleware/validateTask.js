60eb6498eeef159d51be3b9602a882c23cfe6c86
Tiana Turner <tianasturner.17@gmail.com>
2026-04-14 16:55:41 -0400

    add new middleware

--- src/middleware/validateTask.js
+++ src/middleware/validateTask.js
@@ -1,4 +1,4 @@
-import { body } from 'express-validator';
+import { body, query } from 'express-validator';
 import { checkValidationResults } from './handleValidationErrors.js';
 
 export const validateTask = [
@@ -15,6 +15,15 @@ export const validateTask = [
     .optional()
     .isBoolean()
     .withMessage('completed must be true or false'),
+
+  checkValidationResults,
+];
+
+export const validateGetTasks = [
+  query('completed')
+    .optional()
+    .isIn(['true', 'false'])
+    .withMessage('completed query parameter must be true or false'),
 
   checkValidationResults,
 ];
